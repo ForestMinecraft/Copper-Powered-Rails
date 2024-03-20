82551e1a452fce5f9b7012740aae74d348512da0
ForestMinecraft <161991814+ForestMinecraft@users.noreply.github.com>
2024-03-20 11:37:04 -0800

    Initial commit

---
+++ src/main/java/net/forest/copper_rails/CopperPoweredRailsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.forest.copper_rails;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CopperPoweredRailsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
